fd96f48253177012da9e23e6368f36745b35c41d
Reiner21 <Duma3000@mail.ru>
2023-04-05 18:41:28 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".swiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
